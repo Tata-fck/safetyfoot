d1ef4ad11aa3f9c3e0177b52ade304cd7c5c660c
Tata52 <alexyaxelcopca2003@gmail.com>
2025-02-06 00:31:45 -0600

    solo para que el pie de pagina se coloque hasta abajo

--- contacto.php
+++ contacto.php
@@ -2,20 +2,22 @@
 
     <link rel="stylesheet" href="css/contacto.css">
 
-    <div class="cont-form">
-        <div class="info-form">
-            <h2>Contáctanos</h2>
-            <p>Por favor envía tus dudas o comentarios en el siguiente formulario.</p>
-            <a href="#"><i class="fa fa-phone"></i> 55-1234-5678</a>
-            <a href="#"><i class="fa fa-envelope"></i> safetyf@gmail.com</a>
-            <a href="#"><i class="fa fa-map-marked"></i> Mexico City, Mexico</a>
+    <div class="main-content">
+        <div class="cont-form">
+            <div class="info-form">
+                <h2>Contáctanos</h2>
+                <p>Por favor envía tus dudas o comentarios en el siguiente formulario.</p>
+                <a href="#"><i class="fa fa-phone"></i> 55-1234-5678</a>
+                <a href="#"><i class="fa fa-envelope"></i> safetyf@gmail.com</a>
+                <a href="#"><i class="fa fa-map-marked"></i> Mexico City, Mexico</a>
+            </div>
+            <form action="#" autocomplete="off">
+                <input type="text" name="nombre" placeholder="Tu Nombre" class="campo">
+                <input type="emal" name="email" placeholder="Tu Email" class="campo">
+                <textarea id="mensaje" placeholder="Tu Mensaje..."></textarea>
+                <input type="submit" name="enviar" value="Enviar Mensaje" class="btn-enviar">
+            </form>
         </div>
-        <form action="#" autocomplete="off">
-            <input type="text" name="nombre" placeholder="Tu Nombre" class="campo">
-            <input type="emal" name="email" placeholder="Tu Email" class="campo">
-            <textarea id="mensaje" placeholder="Tu Mensaje..."></textarea>
-            <input type="submit" name="enviar" value="Enviar Mensaje" class="btn-enviar">
-        </form>
     </div>
 
 <?php include("elements/footer/footer.html");?>
