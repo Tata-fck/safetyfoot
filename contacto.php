f74257874e5b6c202a401b8d889e700e3e873a4c
Tata52 <alexcopca03@gmail.com>
2025-03-31 11:21:28 -0600

    funcionalidad de pag contacto

--- contacto.php
+++ contacto.php
@@ -7,17 +7,42 @@
             <div class="info-form">
                 <h2>Contáctanos</h2>
                 <p>Por favor envía tus dudas o comentarios en el siguiente formulario.</p>
-                <a href="#"><i class="fa fa-phone"></i> 55-1234-5678</a>
-                <a href="#"><i class="fa fa-envelope"></i> safetyf@gmail.com</a>
-                <a href="#"><i class="fa fa-map-marked"></i> Mexico City, Mexico</a>
+                <a href="https://wa.me/5215613199804" target="_blank"><i class="fa fa-phone"></i> 55-1234-5678</a>
+                <a href="mailto:ventas@grupocapi.com.mx?subject=Razon%20de%20Contacto&body=Nombre: %0AEmpresa: %0ATelefono: %0AComentario: %0A" target="_blank">
+                    <i class="fa fa-envelope"></i> ventas@grupocapi.com.mx </a>
+                <a href="https://maps.app.goo.gl/i3JnuHWJKzqqUZfX6?g_st=com.google.maps.preview.copy"><i class="fa fa-map-marked"></i> C. Juan E. Hernández y Davalos 85b, Algarín, Cuauhtémoc, 06880 Ciudad de México, CDMX</a>
             </div>
-            <form action="#" autocomplete="off">
+            <form id="contactForm" autocomplete="off">
                 <input type="text" name="nombre" placeholder="Tu Nombre" class="campo">
-                <input type="emal" name="email" placeholder="Tu Email" class="campo">
-                <textarea id="mensaje" placeholder="Tu Mensaje..."></textarea>
+                <input type="email" name="email" placeholder="Tu Email" class="campo">
+                <input type="tel" name="celular" placeholder="Tu WhatsApp" class="campo" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+                <textarea id="mensaje" name= "mensaje" placeholder="Tu Mensaje..."></textarea>
                 <input type="submit" name="enviar" value="Enviar Mensaje" class="btn-enviar">
             </form>
+            
+            <p id="responseMessage"></p>
         </div>
     </div>
+
+<script>
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    var formData = new FormData(this);
+
+    fetch("elements/enviar.php", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("responseMessage").innerHTML = data; 
+    })
+    .catch(error => {
+        document.getElementById("responseMessage").innerHTML = "<p style='color:red;'>Error al enviar.</p>";
+    });
+});
+</script>
+
 
 <?php include("elements/footer/footer.html");?>
---
+++ elements/enviar.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = htmlspecialchars($_POST["nombre"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $celular = htmlspecialchars($_POST["celular"]);
+    $mensaje = htmlspecialchars($_POST["mensaje"]);
+
+    $destinatario = "ventas@grupocapi.com.mx";
+    $asunto = "Nuevo mensaje de contacto";
+
+    $cuerpo = "Nombre: $nombre\n";
+    $cuerpo .= "Email: $email\n";
+    $cuerpo .= "WhatsApp: $celular\n";
+    $cuerpo .= "Mensaje:\n$mensaje\n";
+
+    $headers = "From: contacto@clientes.mx\r\nReply-To: $email\n";
+
+    if (mail($destinatario, $asunto, $cuerpo, $headers)) {
+        echo "<p style='color:green;'>¡Mensaje enviado con éxito!</p>";
+    } else {
+        echo "<p style='color:red;'>Error al enviar el mensaje.</p>";
+    }
+} else {
+    echo "Método no permitido.";
+}
+?>
